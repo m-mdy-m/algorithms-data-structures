3dfdcce3c97dd467764926c55b10edb24c9a2a09
mahdi <mahdimamashli1383@gmail.com>
2024-03-17 21:03:32 +0330

    Add Rectangle class and log its area

--- 2.OOP/concepts/BeginnerAnswer/1.class.js
+++ 2.OOP/concepts/BeginnerAnswer/1.class.js
@@ -1,2 +1,12 @@
-'1 :'
+"1 :";
 
+class Rectangle {
+  constructor(width, heigh) {
+    this.width = width;
+    this.height = heigh;
+  }
+  calculateArea() {
+    return this.width * this.height;
+  }
+}
+console.log(`rectangle area: ,${new Rectangle(4,4)}`);
