3361ecff6288b4f8007c766306ebcc4db4898dff
Mahdi <bitsgenix@gmail.com>
2024-03-30 23:20:37 +0330

    "Added example and summary to OverridingAndOverloading markdown, added javadocs to OverridingAndOverloading.java"
    This commit adds a copied example and summary to the OverridingAndOverloading markdown file, as well as javadocs to the OverridingAndOverloading.java file to explain the concepts of overriding and overloading demonstrated in the code. A warning is also added to the markdown file regarding the source of the example.

--- 2.OOP/concepts/MediumExample/OverridingAndOverloading.java
+++ 2.OOP/concepts/MediumExample/OverridingAndOverloading.java
@@ -34,3 +34,29 @@ class MathProcessor extends Processor {
 	}
 
 }
+/**
+ * In the provided example, both overriding and overloading are demonstrated within the `Processor` and `MathProcessor` classes.
+ * !Overriding:
+ * In the `MathProcessor` class, the `process(int i, int j)` method is overridden from its superclass `Processor`. This means that the `process` method in the `MathProcessor` class provides a specific implementation for the method inherited from `Processor`. 
+ **@Override
+ * *public void process(int i, int j) {
+ * *System.out.println("Sum of integers is " + (i + j));
+ * *}
+ *  When the `process` method with two integer parameters is called on an instance of `MathProcessor`, this overridden version of the method will be executed, which calculates and prints the sum of the two integers.
+ * !Overloading:
+ *  In the `Processor` class, there are multiple `process` methods with the same name but different parameter lists. This is method overloading.
+ * 
+ * 
+ * *public void process(int i, int j) { ... }
+ * *public void process(int[] ints) { ... }
+ * *public void process(Object[] objs) { ... }
+ *   These methods have the same name `process`, but they accept different parameters:
+ *  - The first `process` method accepts two integer parameters.
+ *  - The second `process` method accepts an array of integers.
+ *  - The third `process` method accepts an array of objects.
+ *  This allows for flexibility in how the `process` method can be called. Depending on the parameters provided, the appropriate overloaded method will be invoked. 
+ *  In summary:
+ *  - **Overriding** is when a subclass provides a specific implementation for a method that is already defined in its superclass.
+ *  - **Overloading** is when multiple methods with the same name but different parameters exist within the same class.
+ *  These concepts are fundamental in Java and are used to achieve polymorphism and code reuse.
+ */
