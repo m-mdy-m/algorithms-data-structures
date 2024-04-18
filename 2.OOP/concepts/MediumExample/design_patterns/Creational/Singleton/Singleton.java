2310f5a8e923e6bf88658ff909890665f1931fe5
Mahdi <bitsgenix@gmail.com>
2024-04-19 02:36:23 +0330

    Singleton pattern implementation in Go, Java, and Python
    The diff includes three versions of the Singleton pattern implemented in Go, Java, and Python. The Go version uses a mutex lock to ensure thread safety. The Java version is non

---
+++ 2.OOP/concepts/MediumExample/design_patterns/Creational/Singleton/Singleton.java
@@ -0,0 +1,39 @@
+// singleton.java : 
+package refactoring_guru.singleton.example.non_thread_safe;
+
+public final class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        // The following code emulates slow initialization.
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
+/// clint.java
+
+package refactoring_guru.singleton.example.non_thread_safe;
+
+public class DemoSingleThread {
+    public static void main(String[] args) {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+        Singleton singleton = Singleton.getInstance("FOO");
+        Singleton anotherSingleton = Singleton.getInstance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
