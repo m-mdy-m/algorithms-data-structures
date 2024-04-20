f9e21dc6ba79bb770bac6e42c91898c43fdd0ef9
mahdi <mahdimamashli1383@gmail.com>
2024-04-20 10:11:22 +0330

    "Lazy initialization: introduced a private constructor, changed isConnected to a connection promise, and added error handling"

--- 2.OOP/concepts/MediumExample/design_patterns/Creational/Lazy-initialization/Lazy-initialization.ts
+++ 2.OOP/concepts/MediumExample/design_patterns/Creational/Lazy-initialization/Lazy-initialization.ts
@@ -1,16 +1,40 @@
 class Database {
-  private static isConnected: boolean = false;
-  static getConnection() {
-    if (!this.isConnected) {
-      console.log("connecting ...");
-      this.isConnected = true;
+  private static connection: Promise<Database> | null = null;
+
+  private constructor() {}
+
+  // Database connection simulation
+  static async getConnection(): Promise<string> {
+    if (!this.connection) {
+      console.log("Connecting...");
+      try {
+        this.connection = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(new Database());
+          }, 1000);
+        });
+      } catch (error) {
+        console.error("Error establishing database connection:", error);
+        throw error; 
+      }
     }
-    return "Database connection ";
+
+    return (await this.connection).getConnectionMessage();
+  }
+
+  private getConnectionMessage(): string {
+    return "Database connection established";
   }
 }
 
-const connection1 = Database.getConnection();
-console.log(connection1);
+(async () => {
+  try {
+    const connection1 = await Database.getConnection();
+    console.log(connection1);
 
-const connection2 = Database.getConnection();
-console.log(connection2);
+    const connection2 = await Database.getConnection();
+    console.log(connection2);
+  } catch (error) {
+    console.error("An error occurred while using the database:", error);
+  }
+})();
