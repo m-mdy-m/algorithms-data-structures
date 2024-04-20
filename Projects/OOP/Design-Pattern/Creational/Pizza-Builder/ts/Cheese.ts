e039f1cd9c8dabf4e35d607712372025548e755e
mahdi <mahdimamashli1383@gmail.com>
2024-04-20 19:36:02 +0330

    "Refactored Cheese class and updated Pizza interface and builder methods to use CheeseMenu, Crust interface, and updated Sauce and Topping interfaces"

--- Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Cheese.ts
+++ Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Cheese.ts
@@ -1,11 +1,8 @@
-export class Cheese{
-    mozzarella(){
-
-    }
-    cheddar(){
-
-    }
-    vegan(){
-        
-    }
+import { Cheese } from "./Pizza";
+export class CheeseMenu implements Cheese {
+  readonly price: number;
+  readonly type: string;
+  mozzarella() {}
+  cheddar() {}
+  vegan() {}
 }
