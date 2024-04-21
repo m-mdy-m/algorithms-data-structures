986d7aa873b92ed10309160940cca60cb0a119bf
mahdi <mahdimamashli1383@gmail.com>
2024-04-21 12:39:16 +0330

    "Refactor Pizza Builder to use a base pizza class and move utility functions to pizza class"
    This commit includes the following changes:
    
    * The CheeseOption class now extends a new PizzaBase class.
    * The P

--- Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Crust.ts
+++ Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Crust.ts
@@ -1,11 +1,15 @@
-export class Crust{
-    thin(){
-
-    }
-    thick(){
-
-    }
-    stuffed(){
-        
-    }
+interface SizeItem {
+  thin: string;
+  thick: string;
+  stuffed: string;
+}
+export class Crust {
+  size: SizeItem;
+  constructor(size: SizeItem) {
+    this.size = size;
+  }
+  private listMenu():{key:string}
+  thin() {}
+  thick() {}
+  stuffed() {}
 }
