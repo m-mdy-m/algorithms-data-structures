bc2d74f07db33d94ebafc2467df4661942d0eec8
mahdi <mahdimamashli1383@gmail.com>
2024-04-21 13:39:16 +0330

    "Refactor PizzaBase to PizzaTopping; update CheeseOption and Crust to extend PizzaTopping"
    This commit refactors the `PizzaBase` abstract class to `PizzaTopping`, and updates

--- Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Crust.ts
+++ Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Crust.ts
@@ -1,27 +1,7 @@
-import { PizzaBase } from "./Pizza";
+import { PizzaTopping } from "./Pizza";
 
-export class Crust extends PizzaBase {
-  readonly price: any;
-  readonly category: string = "Crust";
+export class Crust extends PizzaTopping {
   constructor(type: string) {
-    super(type);
-    this.price = this.getPrice(this.category) ?? undefined;
-    this.listMenu()
-  }
-  override listMenu(): { [key: string]: Function } {
-    const cheeseTypes: any = this.listCategory(this.category);
-    if (!cheeseTypes) {
-      throw new Error("Failed to retrieve available crust types.");
-    }
-    const methods: { [key: string]: Function } = {};
-    for (const cheeseType of cheeseTypes) {
-      methods[cheeseType as string] = this.createCheeseMethod(cheeseType);
-    }
-    return methods;
-  }
-  override createCheeseMethod(cheeseType: string): Function {
-    return () => ({ type: cheeseType, price: this.price });
+    super(type, "Crust");
   }
 }
-const a = new Crust("mozzarella");
-console.log("a:", a);
