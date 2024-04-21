8a51bb00e747418b5222fed3b71cb8cc993d908e
mahdi <mahdimamashli1383@gmail.com>
2024-04-21 12:45:17 +0330

    "Added Crust class extending PizzaBase with price and type properties; removed deprecated SizeItem interface and methods"

--- Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Crust.ts
+++ Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Crust.ts
@@ -1,15 +1,22 @@
-interface SizeItem {
-  thin: string;
-  thick: string;
-  stuffed: string;
-}
-export class Crust {
-  size: SizeItem;
-  constructor(size: SizeItem) {
-    this.size = size;
+import { PizzaBase } from "./Pizza";
+
+export class Crust extends PizzaBase {
+  readonly price: number;
+  readonly type: string;
+  readonly category: string = "Crust";
+  constructor(type: string) {
+    super(type);
+    this.price = this.getPrice(this.category) ?? 250;
   }
-  private listMenu():{key:string}
-  thin() {}
-  thick() {}
-  stuffed() {}
+  override listMenu(): { [key: string]: Function } {
+    const cheeseTypes = this.listCategory(this.category) ?? [];
+    const methods: { [key: string]: Function } = {};
+    for (const cheeseType of cheeseTypes) {
+      methods[cheeseType as string] = this.createCheeseMethod(cheeseType);
+    }
+    return methods;
+  }
+  override createCheeseMethod(cheeseType: string): Function {
+    return () => ({ type: cheeseType, price: this.price });
+  }
 }
