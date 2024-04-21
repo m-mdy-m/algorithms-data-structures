48ea95fd98448f0f5ed8237a6da8a3cc82e9ca28
Mahdi <bitsgenix@gmail.com>
2024-04-21 12:39:16 +0330

    "Refactor Pizza Builder to use a base pizza class and move utility functions to pizza class"
    This commit includes the following changes:
    
    * The CheeseOption class now extends a new PizzaBase class.
    * The P

--- Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Cheese.ts
+++ Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Cheese.ts
@@ -1,23 +1,23 @@
-import { Cheese } from "./Pizza";
-import { PizzaMenuItem, findPriceByType, listCategory } from "./utils";
-export class CheeseOption implements Cheese {
+import { PizzaBase } from "./Pizza";
+export class CheeseOption extends PizzaBase {
   readonly price: number;
   readonly type: string;
   readonly category: string = "Cheese";
   constructor(type: string) {
+    super(type);
     this.type = type;
-    this.price = findPriceByType(this.category, type) ?? 250;
-    this.listMenu();
+    this.price = this.getPrice(this.category) ?? 250; 
   }
-  private listMenu(): { [key: string]: Function } {
-    const cheeseTypes = listCategory(this.category) ?? [];
-    const methods: { [key: string]: Function } = {}; 
+
+  override listMenu(): { [key: string]: Function } {
+    const cheeseTypes = this.listCategory(this.category) ?? [];
+    const methods: { [key: string]: Function } = {};
     for (const cheeseType of cheeseTypes) {
       methods[cheeseType as string] = this.createCheeseMethod(cheeseType);
     }
     return methods;
   }
-  private createCheeseMethod(cheeseType: string): Function {
+  override createCheeseMethod(cheeseType: string): Function {
     return () => ({ type: cheeseType, price: this.price });
   }
 }
--- Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Pizza.ts
+++ Projects/OOP/Design-Pattern/Creational/Pizza-Builder/ts/Pizza.ts
@@ -1,3 +1,29 @@
+import data from "../price.json";
+// Interface defining the structure of the Pizza Menu data
+// This specifies the expected format of the JSON data
+interface PizzaMenu {
+  Cheese: { type: string; price: number }[];
+  Crust: { type: string; price: number }[];
+  Sauce: { type: string; price: number }[];
+  Topping: { type: string; price: number }[];
+}
+// Function to load a specific menu category (not used in the current implementation)
+// This function checks if the requested category exists in the data
+// and returns the corresponding data if found. It could be useful if you need to
+// access specific categories for other purposes.
+function loadPizzaMenu(category: string):any {
+  if (category in (data as PizzaMenu)) {
+    const typedData = data as PizzaMenu;
+    return Object.getOwnPropertyDescriptor(typedData, category)?.value;
+  }
+  return undefined;
+}
+// Interface defining the structure of a single Pizza Menu item
+// This specifies the properties expected for each item within a category
+interface PizzaMenuItem {
+  type: string;
+  price: number;
+}
 export interface Crust {
   type: string;
   price: number;
@@ -24,4 +50,33 @@ export interface Pizza {
   cheese?: Cheese;
   toppings: Topping[];
   calculatePrice: number;
+}
+export abstract class PizzaBase {
+  readonly type: string;
+  constructor(type:string) {
+    this.type = type
+  }
+  abstract listMenu(): { [key: string]: Function };
+  // Function to find the price of a specific item type within a category
+  // This function takes the category (e.g., "Cheese") and item type (e.g., "mozzarella")
+  // as arguments. It checks if the category exists, then uses the `find` method
+  // to locate the item with the matching type within that category. If found,
+  // it returns the price of the item, otherwise it returns undefined.
+  protected getPrice(category: string) {
+    if (category in (data as PizzaMenu)) {
+      const menu = loadPizzaMenu(category);
+      const priceInfo = menu?.find(
+        (item: PizzaMenuItem) => item?.type === this.type
+      );
+      return priceInfo?.price;
+    }
+  }
+  protected listCategory(category: string) {
+    if (category in (data as PizzaMenu)) {
+      const menu = loadPizzaMenu(category);
+      const Info = menu?.map((f: PizzaMenuItem) => f?.type);
+      return Info;
+    }
+  }
+  abstract createCheeseMethod(cheeseType: string): Function 
 }
