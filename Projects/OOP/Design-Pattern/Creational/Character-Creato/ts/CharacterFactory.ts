e5aa068dc0ef3e91de7a4b868238ec19b153b89f
mahdi <mahdimamashli1383@gmail.com>
2024-04-21 23:26:41 +0330

    "Fixed typo: Added semicolon to properly initialize protected property in abstract class"

--- Projects/OOP/Design-Pattern/Creational/Character-Creato/ts/CharacterFactory.ts
+++ Projects/OOP/Design-Pattern/Creational/Character-Creato/ts/CharacterFactory.ts
@@ -2,7 +2,7 @@ import { Soldier } from "./Soldier";
 import { InfoSoldier } from "./helper";
 import { SoldierData } from "./interface";
 export abstract class CharacterFactory<T extends Soldier> {
-  protected readonly infoSoldier: SoldierData=InfoSoldier
+  protected readonly infoSoldier: SoldierData = InfoSoldier;
   protected abstract createSoldierImpl(): T;
 
   public createSoldier(): T {
