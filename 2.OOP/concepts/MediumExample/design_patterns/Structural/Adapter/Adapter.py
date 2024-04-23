82496129a6ffcf492401bbd5befab75b302d5d73
mahdi <mahdimamashli1383@gmail.com>
2024-04-23 22:38:42 +0330

    add python example

---
+++ 2.OOP/concepts/MediumExample/design_patterns/Structural/Adapter/Adapter.py
@@ -0,0 +1,36 @@
+import math
+class Shape:
+    def __init__(self):
+        pass
+    def getArea(self):
+        pass
+class AreaMeasurable:
+    def __init__(self):
+        pass
+    def getAreaInCm(self):
+        pass
+class Square(Shape):
+    def __init__(self,side):
+        self.side =side
+    def getArea(self):
+        return self.side * self.side
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def getArea(self):
+        return math.pi * self.radius * self.radius
+
+class ShapeAreaCalculator(AreaMeasurable):
+    def __init__(self,shape):
+        self.shape = shape
+    def getAreaInCm(self):
+        return self.shape.getArea() * 2.54 # This factor is used to convert the area from inches (assumed unit) to centimeters.
+
+square = Square(5)
+circle = Circle(3)
+square_adapter = ShapeAreaCalculator(square)
+circle_adapter = ShapeAreaCalculator(circle)
+
+print(f'Area of square in cm: {square_adapter.getAreaInCm()}')
+print(f'Area of circle in cm: {circle_adapter.getAreaInCm()}')
