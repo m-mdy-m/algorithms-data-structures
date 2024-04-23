87329a1a3f60a19f2138a099894aa45c7597d98b
Mahdi <bitsgenix@gmail.com>
2024-04-23 23:33:55 +0330

    "Added concrete types Shape, AreaMeasurable, Square, Circle and ShapeAreaCalculator; implemented getArea method for Square and Circle; implemented getAreaInCm method for ShapeAreaCalculator; removed unused import

--- 2.OOP/concepts/MediumExample/design_patterns/Structural/Adapter/Adapter.go
+++ 2.OOP/concepts/MediumExample/design_patterns/Structural/Adapter/Adapter.go
@@ -1,10 +1,44 @@
 package main
-import "fmt"
-// type Shape struct {
-// 	number getArea
-// }
-// type AreaMeasurable {
-// 	number getAreaInCm;
-// }
 
-fmt.println("hello Go")
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	getArea() float64
+}
+
+type AreaMeasurable interface {
+	getAreaInCm() float64
+}
+
+type Square struct {
+	Side float64
+}
+
+func (this *Square) GetArea() float64 {
+	return this.Side * this.Side
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (this *Circle) GetArea() float64 {
+	return math.Pi * this.Radius * this.Radius
+}
+
+type ShapeAreaCalculator struct {
+	shape Shape
+}
+func (this *ShapeAreaCalculator) getAreaInCm () float64{
+	return this.shape.getArea() * 2.54
+}
+func main() {
+	square := Square{Side: 5}
+
+	// circle := Circle(5);
+    fmt.Printf("Area of square: %.2f", square.GetArea())
+}
+
