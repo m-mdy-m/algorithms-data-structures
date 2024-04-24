6cdc927dda32adafa690aa931a87ac1d36e40f37
mahdi <mahdimamashli1383@gmail.com>
2024-04-24 12:53:47 +0330

    "Added abstract classes and methods for Shape and AreaMeasurable in Adapter pattern and modified ShapeAreaCalculator to implement AreaMeasurable in Bridge pattern"

--- 2.OOP/concepts/MediumExample/design_patterns/Structural/Adapter/Adapter.py
+++ 2.OOP/concepts/MediumExample/design_patterns/Structural/Adapter/Adapter.py
@@ -1,20 +1,35 @@
+from abc import ABC,abstractmethod
 import math
-class Square:
+class Shape(ABC):
+  """Abstract class representing a shape"""
+  @abstractmethod
+  def getArea(self) -> int:
+    """Abstract method to calculate the area of the shape"""
+    pass
+
+class AreaMeasurable(ABC):
+  """Abstract class defining interface for getting area in cm"""
+  @abstractmethod
+  def getAreaInCm(self) -> int:
+    """Abstract method to get the area in centimeters"""
+    pass
+
+class Square(Shape):
     def __init__(self,side):
         self.side =side
-    def getArea(self):
+    def getArea(self) -> int:
         return self.side * self.side
 
-class Circle:
+class Circle(Shape):
     def __init__(self,radius):
         self.radius = radius
-    def getArea(self):
+    def getArea(self) -> int:
         return math.pi * self.radius * self.radius
 
-class ShapeAreaCalculator:
+class ShapeAreaCalculator(AreaMeasurable):
     def __init__(self,shape):
         self.shape = shape
-    def getAreaInCm(self):
+    def getAreaInCm(self) -> int:
         return self.shape.getArea() * 2.54 # This factor is used to convert the area from inches (assumed unit) to centimeters.
 
 square = Square(5)
