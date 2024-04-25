d68472e96a310ffd35107acc306c6f283b23a4ac
Mahdi <bitsgenix@gmail.com>
2024-04-25 15:30:27 +0330

    Proxy pattern implementation in Go, Python and TypeScript
    The diff shows the implementation of the Proxy pattern in three different programming languages: Go, Python, and TypeScript. The Proxy pattern is used to provide a surrogate or

---
+++ 2.OOP/concepts/MediumExample/design_patterns/Structural/Proxy/Proxy.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+class Subject(ABC):
+    @abstractmethod
+    def request(self) -> None:
+        pass
+
+class RealSubject(Subject):
+    def request(self) -> None:
+        print("RealSubject: Handling request.")
+
+
+class Proxy(Subject):
+    def __init__(self, real_subject: RealSubject) -> None:
+        self._real_subject = real_subject
+
+    def request(self) -> None:
+
+        if self.check_access():
+            self._real_subject.request()
+            self.log_access()
+
+    def check_access(self) -> bool:
+        print("Proxy: Checking access prior to firing a real request.")
+        return True
+
+    def log_access(self) -> None:
+        print("Proxy: Logging the time of request.", end="")
+
+
+def client_code(subject: Subject) -> None:
+    subject.request()
+
+
+if __name__ == "__main__":
+    print("Client: Executing the client code with a real subject:")
+    real_subject = RealSubject()
+    client_code(real_subject)
+
+    print("")
+
+    print("Client: Executing the same client code with a proxy:")
+    proxy = Proxy(real_subject)
+    client_code(proxy)
--- 2.OOP/concepts/MediumExample/design_patterns/Structural/Proxy/Proxy.ts
+++ 2.OOP/concepts/MediumExample/design_patterns/Structural/Proxy/Proxy.ts
@@ -39,9 +39,6 @@ function clientCode(subject: Subject) {
 console.log('Client: Executing the client code with a real subject:');
 const realSubject = new RealSubject();
 clientCode(realSubject);
-
-console.log('');
-
 console.log('Client: Executing the same client code with a proxy:');
 const proxy = new ProxyExample(realSubject);
 clientCode(proxy);
