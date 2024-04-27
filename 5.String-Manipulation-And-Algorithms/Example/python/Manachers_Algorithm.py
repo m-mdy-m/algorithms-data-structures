7f228d22c0acbacea00b8d167c511ffcf42b18db
mahdi <mahdimamashli1383@gmail.com>
2024-04-27 23:57:11 +0330

    "Added example and explanation for Manacher's Algorithm in Python"
    The diff includes the addition of an example and detailed explanation of Manacher's Algorithm for finding the longest palindrome in a string, implemented in Python.

--- 5.String-Manipulation-And-Algorithms/Example/python/Manachers_Algorithm.py
+++ 5.String-Manipulation-And-Algorithms/Example/python/Manachers_Algorithm.py
@@ -40,5 +40,26 @@ if __name__ == "__main__":
     print("Manachers-function:", manachers_values)
 
 """
+| Starting Index (i) | Length (P[i]) | Palindrome |            Diagram                 | Explanation |                                                                                                     |
+|--------------------|---------------|------------|----------------------------------- |-------------|-----------------------------------------------------------------------------------------------------| 
+| 0                  | 1             | A          | A#                                 | Single character palindrome centered at index 0 (A).                                                              |
+| 0                  | 3             | ABA        | ^#A#B#A#                           | Longest palindrome centered at index 0, extending outwards to "ABA".                                              |
+| 1                  | 3             | BAB        | #A#B#A#^                           | Longest palindrome centered at index 1, extending outwards to "BAB". (Overlaps with the previous palindrome)      |
+| 3                  | 1             | D          | #A#B#D#^                           | Single character palindrome centered at index 3 (D).                                                              |
+| 4                  | 1             | A          | #A#B#D#A#^                         | Single character palindrome centered at index 4 (A).                                                              |
+| 5                  | 1             | B          | #A#B#D#A#B#^                       | Single character palindrome centered at index 5 (B).                                                              |
+| 5                  | 3             | BAB        | #A#B#D#A#B#^                       | Longest palindrome centered at index 5, extending outwards to "BAB". (Overlaps with the previous two palindromes) |
+| 7                  | 1             | D          | #A#B#D#A#B#^#                      | Single character palindrome centered at index 7 (D).                                                              |
+| 8                  | 1             | A          | #A#B#D#A#B#A#^                     | Single character palindrome centered at index 8 (A).                                                              |
+| 9                  | 1             | C          | #A#B#D#A#B#A#C#^                   | Single character palindrome centered at index 9 (C).                                                              |
+| 10                 | 1             | B          | #A#B#D#A#B#A#C#B#^                 | Single character palindrome centered at index 10 (B).                                                             |
+| 10                 | 3             | ABA        | #A#B#D#A#B#A#C#B#A#^               | Longest palindrome centered at index 10, extending outwards to "ABA". (Overlaps with the previous palindrome)     |
+| 11                 | 3             | BAB        | #A#B#D#A#B#A#C#B#A#B#^             | Longest palindrome centered at index 11, extending outwards to "BAB". (Overlaps with the previous two palindromes)|
+| 13                 | 1             | A          | #A#B#D#A#B#A#C#B#A#B#A#^           | Single character palindrome centered at index 13 (A).                                                             |
+| 14                 | 1             | B          | #A#B#D#A#B#A#C#B#A#B#A#B#^         | Single character palindrome centered at index 14 (B).                                                             |
+| 15                 | 1             | A          | #A#B#D#A#B#A#C#B#A#B#A#B#A#^       | Single character palindrome centered at index 15 (A).                                                             |
+| 15                 | 3             | ABA        | #A#B#D#A#B#A#C#B#A#B#A#B#A#A#^     | Longest palindrome centered at index 15, extending outwards to "ABA". (Overlaps with the previous palindrome)     |
+| 16                 | 3             | BAB        | #A#B#D#A#B#A#C#B#A#B#A#B#A#A#B#^   | Longest palindrome centered at index 16, extending outwards to "BAB".                                             |
+| 18                 | 1             | B          | #A#B#D#A#B#A#C#B#A#B#A#B#A#A#B#B#^ | Single character palindrome centered at index 18 (B).                                                             |
 
 """
