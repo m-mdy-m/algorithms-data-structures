70e2e526ce1b8420ee94f51f07c41aabaf557c80
Mahdi <bitsgenix@gmail.com>
2024-04-27 20:31:17 +0330

    "Add Z-algorithm implementation in JavaScript"
    This diff introduces a new class `ZAlgorithm` that calculates the Z-function for a given input string. The Z-function is a powerful tool in string matching and pattern searching algorithms

---
+++ 5.String-Manipulation-And-Algorithms/Example/js/Z_algorithm.js
@@ -0,0 +1,35 @@
+class ZAlgorithm {
+  constructor(text) {
+    this.text = text;
+    this.n = text.length;
+    this.z_values = new Array(this.n).fill(0);
+  }
+
+  calculate() {
+    let l = 0, r = 0;
+    for (let i = 1; i < this.n; i++) {
+      if (i <= r) {
+        let k = i - l;
+        if (this.text[i] == this.text[k]) {
+          this.z_values[i] = Math.min(r - i + 1, this.z_values[k]);
+        } else {
+          l = i;
+          r = i + this.z_values[k] - 1;
+        }
+      } else {
+        l = r = i;
+        while (r < this.n && this.text[r] == this.text[r - 1]) {
+          r += 1;
+        }
+        this.z_values[i] = r - l - 1;
+      }
+    }
+    return this.z_values;
+  }
+}
+const text = "ABABDABACDABABCABAB";
+const z_algorithm = new ZAlgorithm(text);
+const z_values = z_algorithm.calculate();
+
+console.log("Text:", text);
+console.log("Z-function:", z_values);
