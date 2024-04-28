255136d783dcb934ca2b5825421bd6c9a37a3047
mahdi <mahdimamashli1383@gmail.com>
2024-04-28 09:59:22 +0330

    Update ManachersAlgorithm class: - Change type of C and P properties from Array<number> to number[] - Update calculate method return type from Array<number[]> to number[][]

--- 5.String-Manipulation-And-Algorithms/Example/ts/Manachers_Algorithm.ts
+++ 5.String-Manipulation-And-Algorithms/Example/ts/Manachers_Algorithm.ts
@@ -1,8 +1,8 @@
 class ManachersAlgorithm {
   text: string;
   processed_text: string;
-  C: Array<number>;
-  P: Array<number>;
+  C: number[];
+  P: number[];
   C_center: number;
   R: number;
   constructor(text: string) {
@@ -13,7 +13,7 @@ class ManachersAlgorithm {
     this.C_center = 0;
     this.R = 0;
   }
-  calculate(): Array<number[]>  {
+  calculate(): number[][] {
     for (let i = 1; i < this.processed_text.length - 1; i++) {
       const i_mirror = 2 * this.C_center - i;
       if (i <= this.R) {
@@ -21,28 +21,30 @@ class ManachersAlgorithm {
       } else {
         this.P[i] = 0;
       }
+
       while (
         i - this.P[i] - 1 >= 0 &&
         i + this.P[i] + 1 < this.processed_text.length &&
         this.processed_text[i - this.P[i] - 1] ===
-          this.processed_text[i - this.P[i] + 1]
+          this.processed_text[i + this.P[i] + 1]
       ) {
-        this.P[i] += 1;
+        this.P[i]++;
       }
+
       if (i + this.P[i] > this.R) {
         this.C_center = i;
         this.R = i + this.P[i];
       }
     }
-    const palindrome: Array<number[]> = [];
-    for (let i = 0; i < this.processed_text.length - 1; i += 2) {
+    const palindromes: number[][] = [];
+    for (let i = 1; i < this.processed_text.length - 1; i += 2) {
       if (this.P[i] > 0) {
         const startIndex = (i - this.P[i]) / 2;
         const length = this.P[i];
-        palindrome.push([startIndex, length]);
+        palindromes.push([startIndex, length]);
       }
     }
-    return palindrome;
+    return palindromes;
   }
 }
 const textInput = "ABABDABACDABABCABAB";
@@ -52,3 +54,43 @@ const manachersValues = manachers.calculate();
 console.log("Text:", textInput);
 console.log("Manachers-function:", manachersValues);
 console.log("processed_text:", manachers.processed_text);
+/**
+- > output : 
+
+Text: ABABDABACDABABCABAB
+Manachers-function: [
+  [ 0, 1 ],  [ 0, 3 ],  [ 1, 3 ],
+  [ 3, 1 ],  [ 4, 1 ],  [ 5, 1 ],
+  [ 5, 3 ],  [ 7, 1 ],  [ 8, 1 ],
+  [ 9, 1 ],  [ 10, 1 ], [ 10, 3 ],
+  [ 11, 3 ], [ 13, 1 ], [ 14, 1 ],
+  [ 15, 1 ], [ 15, 3 ], [ 16, 3 ],
+  [ 18, 1 ]
+]
+processed_text: #A#B#A#B#D#A#B#A#C#D#A#B#A#B#C#A#B#A#B#-> length : 39
+---
+
+
+| Index (i) | Processed Text Slice | Start Index (original text) | Length | Reason |
+|-----------|----------------------|-----------------------------|--------|--------|
+| (0, 1)    | `#` | 0              | 1                           | Single character 'A' at index 0 is a palindrome of length 1. |
+| (0, 3)    | `#A#B#`              | 0                           | 3      | Palindrome "ABA" centered at index 1 (considering `#` as boundaries). |
+| (1, 3)    | `#A#`                | (1 - 3) // 2 = -1 (ignored) | 3      | Center of this palindrome is at index 1, but it extends beyond the beginning of the processed text (invalid). |
+| (3, 1)    | `#B#A#`              | 3 // 2 = 1                  | 1      | Single character 'B' at index 1 is a palindrome of length 1. |
+| (4, 1)    | `#A#B#`              | 4 // 2 = 2                  | 1      | Single character 'A' at index 2 is a palindrome of length 1. |
+| (5, 1)    | `#D#A#`              | 5 // 2 = 2                  | 1      | Single character 'D' at index 2 is a palindrome of length 1. |
+| (5, 3)    | `#D#A#B#`            | 5 // 2 = 2                  | 3      | Palindrome "BAB" centered at index 3. |
+| (7, 1)    | `#A#C#`              | 7 // 2 = 3                  | 1      | Single character 'C' at index 3 is a palindrome of length 1. |
+|-----------|----------------------|-----------------------------|--------|--------|
+| (18, 1)   | `#B#`                | 18 // 2 = 9                 | 1      | Single character 'B' at index 9 is a palindrome of length 1. |
+
+```
+      i (center)
+   0 # A # B #  #  #  #  #
+   -3-2-1--0--1--2--3--4-
+          R (right boundary)
+          P[i] = 1
+```
+```
+
+ */
