f0d7ae57fd4e25babd7a1ae6ece54904e48622c1
mahdi <mahdimamashli1383@gmail.com>
2024-06-06 08:17:20 +0330

    format save

--- 7.Search-Algorithm/Example/Linear/Linear.js
+++ 7.Search-Algorithm/Example/Linear/Linear.js
@@ -1,10 +1,10 @@
-const numbers = [10,2,4,1,5]
+const numbers = [10, 2, 4, 1, 5];
 
 function linearSearch(array, num) {
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] === num) {
-        return i;
-      }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === num) {
+      return i;
     }
-    return -1;
   }
+  return -1;
+}
