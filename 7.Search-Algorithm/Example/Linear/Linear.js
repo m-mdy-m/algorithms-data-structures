f3c41bcf39369cf59a479edf4154f6f125bde101
Mahdi <bitsgenix@gmail.com>
2024-06-06 08:13:51 +0330

    add example linear example for js

---
+++ 7.Search-Algorithm/Example/Linear/Linear.js
@@ -0,0 +1,10 @@
+const numbers = [10,2,4,1,5]
+
+function linearSearch(array, num) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === num) {
+        return i;
+      }
+    }
+    return -1;
+  }
