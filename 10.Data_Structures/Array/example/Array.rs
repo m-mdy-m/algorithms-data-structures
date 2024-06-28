ab3ea875de3c0abee2815893b95dc5a0ff8eaa93
Mahdi <bitsgenix@gmail.com>
2024-06-29 00:28:32 +0330

    feat(Array): "Added array examples in JavaScript, Python, Rust, and TypeScript"

---
+++ 10.Data_Structures/Array/example/Array.rs
@@ -0,0 +1,4 @@
+fn main(){
+    let ArrayDemo: [i32; 5] = [10, 20, 30, 40, 50];
+    println!("{:?}",ArrayDemo)
+}
---
+++ 10.Data_Structures/Array/example/Array.ts
@@ -0,0 +1,2 @@
+const ArrayDemo:number[] = [10,20,30,40,50]
+console.log(ArrayDemo)
