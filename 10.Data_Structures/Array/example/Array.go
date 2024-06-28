0262a71a0c9eefcba3d4ca99cb6a5a2234256940
Mahdi <bitsgenix@gmail.com>
2024-06-28 23:46:11 +0330

    feat(Array): "Added Go and Java examples for arrays"

---
+++ 10.Data_Structures/Array/example/Array.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	array := [5]int32{10, 20, 30, 40, 50}
+	fmt.Println(array)
+}
---
+++ 10.Data_Structures/Array/example/Array.java
@@ -0,0 +1,15 @@
+public class Array {
+    public static void main(String[] args){
+    int[] ArrayInt = new int[5];
+    ArrayInt[0] = 10;
+        ArrayInt[1] = 20;
+        ArrayInt[2] = 30;
+        ArrayInt[3] = 40;
+        ArrayInt[4] = 50;
+    System.out.println("Array Index 0: " + ArrayInt[0]);
+    System.out.println("Array Index 1: " + ArrayInt[1]);
+    System.out.println("Array Index 2: " + ArrayInt[2]);
+    System.out.println("Array Index 3: " + ArrayInt[3]);
+    System.out.println("Array Index 4: " + ArrayInt[4]);
+    }
+}
